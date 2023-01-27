c9144c7c4c31ab58e574beef028c6de5d9345d21
Frank de Boer <deboerfp@gmail.com>
2023-01-27 11:48:18 +0100

    added header and about me page

---
+++ pages/over-mij.vue
@@ -0,0 +1,8 @@
+<script setup>
+const story = await useStoryblok('over-mij', {
+    version: "draft"});
+</script>
+
+<template>
+   <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+</template>
